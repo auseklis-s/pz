4e5800534d423fec3dca27e498888a83f632c006
auseklis-s <sogoyn.p@gmail.com[student@A420-09 ]$ git config --global user.name>
2024-02-28 09:08:37 +0300

    pz

--- pz11/pz11_2.py
+++ pz11/pz11_2.py
@@ -1,13 +1,26 @@
-# with open('text18-23.txt', 'r', encoding='utf-8') as file:
-#     content = file.readlines()
+# Из предложенного текстового файла (text18-23.txt) вывести на экран его содержимое,
+# количество знаков пунктуации в первых четырёх строках. Сформировать новый файл, в
+# который поместить текст в стихотворной форме предварительно заменив символы верхнего
+# регистра на нижний.
 
-a = open('text18-23.txt', 'r', encoding='utf-16')
-content = a.readlines()
+al = open('pz11/text18-23.txt', 'r', encoding='utf-16')
+
+content = al.readlines()
+
+
+for line_n, line in enumerate(content):
+    print(line)
+
 cont = content[:4]
-print(*a)
 punctuation = 0
 for i in cont:
     for y in i:
         if y in ",.:;?!":
             punctuation +=1
-print(punctuation)
+print('В первых четырех строках ',punctuation ,'знака пунктуации')
+
+ne = open('pz11/stix.txt', 'w', encoding='utf-16')
+
+for y in content:
+    form_al = y.lower()
+    ne.write(form_al)
