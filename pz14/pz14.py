0795449e7c16f57824aa4a087a5f0ce82ba93454
auseklis-s <vova13062006@bk.ru>
2024-04-18 00:22:59 +0300

    pz

--- pz14/pz14.py
+++ pz14/pz14.py
@@ -3,12 +3,12 @@ import re
 count1 = 0
 count2 = 0
 
-with open ('ip_address.txt' , 'r') as file1, open('output_file1.txt' , 'w') as file2, open('output_file2.txt', 'w') as file3:
+with open ('pz14/ip_address.txt' , 'r') as file1, open('pz14/output_file1.txt' , 'w') as file2, open('pz14/output_file2.txt', 'w') as file3:
     for i in file1:
         if re.match(r'\d{1,3}\.\d{1,3}', i):
-            print(i)
             test = i.strip().split('.')
-            if int(test[0])>0 and int(test[1])>1:
+            print(test)
+            if int(test[0])>0 or int(test[1])>0:
                 file2.write(i)
                 count1 += 1
             else:
